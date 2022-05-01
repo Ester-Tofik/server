7966f573fa998369519b56b2061454b376bb652f
אילה פרידמן <325191575@mby.co.il>
2022-05-01 18:42:39 +0300

    UserSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,47 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var UserSchema = new Schema({
-	'firstName' : String,
-	'lastName' : String,
-	'id' : String,
-	'email' : String,
-	'password' : String,
-	'phoneNumber' : Number,
-	'birthDate' : Date,
-	'medicines' : Array
+const medicineSchema = new mongoose.Schema({
+	apiId: {
+		type: String,
+		minlength: 6,
+		maxlength: 6
+	},
+	name: {
+		type: String
+	},
+	ammount: {
+		type: Number
+	},
+	numberForDay: {
+		type: Number,
+		min: 1
+	},
+	numberOf : {
+		type: Number,
+		min : 10,
+		max : 60
+	},
+	startTakingDate : {
+		type : Date,
+		default : new Date()
+	},
+	SendAReminder : {
+		type : Boolean,
+		default : false
+	}
+})
+
+
+const UserSchema = new Schema({
+	'firstName': String,
+	'lastName': String,
+	'id': String,
+	'email': String,
+	'password': String,
+	'phoneNumber': Number,
+	'birthDate': Date,
+	'medicines': Array
 });
 
 module.exports = mongoose.model('User', UserSchema);
