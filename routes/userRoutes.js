dd0aa8c6ea5a0e1db6f77607bf73e7c1c1285c44
אסתר טופיק <325202349@mby.co.il>
2022-05-10 16:45:43 +0300

    put user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const UserController = require('../controllers/UserController');
 // router.get('/', UserController.list);
 router.get('/:id/:password', UserController.show);
 router.post('/', UserController.createNewUser);
-// router.put('/:id', UserController.update);
+router.put('/:id', UserController.putUser);
 // router.delete('/:id', UserController.remove);
 
 module.exports = router;
