5bc154f4dae027f31293a017782e3cb80383e629
אילה פרידמן <325191575@mby.co.il>
2022-05-15 21:11:03 +0300

    reminder get!😊😂🤣

--- controllers/reminderController.js
+++ controllers/reminderController.js
@@ -16,10 +16,21 @@ module.exports.createNewReminder = async function (req, res, next) {
         const reminderToUpdate = { $set: { "reminderManagement": newReminder }};
         const update = { _id: ObjectId(idForUpdate) };
         const userWithReminder = await userModel.updateOne(update, reminderToUpdate);
-        res.send(userWithReminder);
-
+        res.send(update);
+        
     }
         catch (err) {
+            next(err);
+        }
+    }
+
+    module.exports.getReminderManagement = async function (req, res, next) {
+        try {
+            const id = req.params.id;
+            const user = await userModel.findOne({ _id: id});
+            res.send(user);
+        }
+        catch(err){
             next(err);
         }
     }
--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const reminderController = require('../controllers/reminderController');
 
 router.put('/:id', reminderController.createNewReminder);
-
+router.get('/:id',reminderController.getReminderManagement);
 
 module.exports = router;
